de63fb789168b81dd7a491b5f6a718ae98c6770a
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-09-30 21:35:15 -0700

    worked with tutor to configure colorwrapper, localstorage, etc

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 
 $(document).ready(function () {
-var currentDay = moment().format("dddd");
+// var currentDay = moment().format("dddd");
 var currentHour = moment().hours();
-var dayMoYr = moment().format("MMM Do YY");
-var allTime = (day + ", " + dayMoYr);
+// var dayMoYr = moment().format("MMM Do YY");
+// var allTime = (day + ", " + dayMoYr);
 
     function createColors() {
         var colorWrapper = $("#colorWrapper");
-        for (var i = 16; ready <= 24; i++) {
-            var timeBlock = $("#" + i)
-            var textBoxes = $(".description");
-            var saveBtn = $(".saveBtn");
+        $(".time-block").each(function() {
             var blockHour = parseInt($(this).attr("id"))
+            $(this).children(".hours").text(blockHour)
+            console.log(blockHour, currentHour)
             if(currentHour < blockHour) {
                 $(this).addClass("past") 
             } else if(currentHour === blockHour) {
                 $(this).addClass("present")
             } else {$(this).addClass("future")}
-        }
+        })
     }
     createColors();
 
     $(".saveBtn").on("click", function() {
         var value = $(this).siblings(".description").val()
         var time = $(this).parent().attr("id")
-        localStorage()
         var todos = JSON.parse(window.localStorage.getItem("todos")) || []
         todos.push({time, value});
         window.localStorage.setItem("todos", JSON.stringify(todos))
-        
+        var savedData = JSON.parse(window.localStorage.getItem("todos"))
+        console.log(savedData)
+        for (let i = 0; i < savedData.length; i++) {
+            var id = parseInt($(this).attr("id"))
+            var element = savedData[i]
+            if(id = element.time) {
+                $(this).text(element.value)
+            }
+        }
 
     })
-
 })
